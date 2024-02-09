5b03a9c3649e677955f775742d85bb841db0f640
Son Nguyen <dev.lamson@gmail.com>
2024-02-09 12:20:15 -0800

    render notion blocks in blog post

--- src/utils/blogEntryFromNotionPost.ts
+++ src/utils/blogEntryFromNotionPost.ts
@@ -1,6 +1,12 @@
 import { type CollectionEntry } from "astro:content";
 import type { Post } from "@lib/interfaces";
 import { slugifyStr } from "./slugify";
+import { extractTargetBlocks } from "@lib/blog-helpers";
+import {
+  downloadFile,
+  getBlock,
+  getAllBlocksByBlockId,
+} from "@lib/notion/client";
 
 export function blogEntryFromNotionPost(post: Post): CollectionEntry<"blog"> {
   return {
@@ -23,4 +29,64 @@ export function blogEntryFromNotionPost(post: Post): CollectionEntry<"blog"> {
       // canonicalURL: "",
     },
   };
+}
+
+export async function queryNotionPostBlocks({ pageId }: { pageId: string }) {
+  if (!pageId) {
+    return [];
+  }
+  // Get all blocks
+  const blocks = await getAllBlocksByBlockId(pageId);
+
+  const fileAtacchedBlocks = extractTargetBlocks("image", blocks)
+    .concat(extractTargetBlocks("file", blocks))
+    .filter(block => {
+      if (!block) {
+        return false;
+      }
+      const imageOrFile = block.Image || block.File;
+      return imageOrFile && imageOrFile.File && imageOrFile.File.Url;
+    });
+
+  // Download files
+  await Promise.all(
+    fileAtacchedBlocks
+      .map(async block => {
+        const imageOrFile = block.Image || block.File;
+        if (
+          !imageOrFile ||
+          !imageOrFile.File ||
+          !imageOrFile.File.Url ||
+          !imageOrFile.File.ExpiryTime
+        ) {
+          console.log("Invalid file URL");
+          return Promise.reject();
+        }
+        const expiryTime = imageOrFile.File.ExpiryTime;
+        if (Date.parse(expiryTime) > Date.now()) {
+          return Promise.resolve(block);
+        }
+        return getBlock(block.Id);
+      })
+      .map(promise =>
+        promise.then(block => {
+          let url!: URL;
+          try {
+            const imageOrFile = block.Image || block.File;
+            if (!imageOrFile || !imageOrFile.File || !imageOrFile.File.Url) {
+              console.log("Invalid file URL");
+              return Promise.reject();
+            }
+            url = new URL(imageOrFile.File.Url);
+          } catch (err) {
+            console.log("Invalid file URL");
+            return Promise.reject();
+          }
+          return Promise.resolve(url);
+        })
+      )
+      .map(promise => promise.then(downloadFile))
+  );
+
+  return blocks;
 }
